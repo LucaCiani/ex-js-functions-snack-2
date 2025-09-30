bb74fff5c5c6e2458535550aa25702fec32232da
LucaCiani <lucaciani88@gmail.com>
2025-09-30 17:29:44 +0200

    snack 7 complete

---
+++ snack-7/main.js
@@ -0,0 +1,17 @@
+let contatore = 10;
+
+function eseguiEferma(messaggio, intervallo, durata) {
+    console.log(messaggio);
+
+    const intervalloId = setInterval(() => {
+        contatore--;
+        console.log(contatore);
+    }, intervallo);
+
+    setTimeout(() => {
+        clearInterval(intervalloId);
+        console.log("Tempo scaduto!");
+    }, durata);
+}
+
+eseguiEferma(`Inizio a contare da ${contatore}`, 1000, 10500);
